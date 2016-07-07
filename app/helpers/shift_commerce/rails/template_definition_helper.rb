101e6638751a293c68cca4ee73113a7737e18519
Peter Hicks <peter@hicks.im>
2016-07-07 14:33:51 +0100

    Pass extra render_template_for arguments to render

--- app/helpers/shift_commerce/rails/template_definition_helper.rb
+++ app/helpers/shift_commerce/rails/template_definition_helper.rb
@@ -1,10 +1,12 @@
 module ShiftCommerce
   module Rails
     module TemplateDefinitionHelper
-      def render_template_for(item)
+      def render_template_for(item, *args)
         tpl = item.template_definition
         if(tpl.present?)
-          render "template_definitions/#{tpl.reference}"
+          render "template_definitions/#{tpl.reference}", *args
+        else
+          render *args
         end
       end
     end
