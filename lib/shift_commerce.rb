3ac20b5e76cd3fdb43f9902afcacb1cb24d6d2ae
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-10-11 13:43:37 +0100

    Removed (broken) error handling

--- lib/shift_commerce.rb
+++ lib/shift_commerce.rb
@@ -1,7 +1,6 @@
 require 'shift_commerce/version'
 
 module ShiftCommerce
-  autoload :BetterErrorPage, File.expand_path(File.join('better_error_page'), __dir__)
 end
 
 require 'shift_commerce/engine'
