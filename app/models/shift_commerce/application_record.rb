cc8436b9d9e13657a0cc61229d0787da4b798825
Peter Hicks <peter@hicks.im>
2016-10-12 10:31:18 +0100

    Remove unnecessary features and Rails 5-only functionality.

--- app/models/shift_commerce/application_record.rb
+++
@@ -1,5 +0,0 @@
-module Shiftcommerce
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -5,13 +5,11 @@ require_relative 'boot'
 require "action_controller/railtie"
 require "action_view/railtie"
 # require "action_mailer/railtie"
-require "active_job/railtie"
-# require "action_cable/engine"
 # require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
-require "shiftcommerce"
+require "shiftcommerce-rails"
 
 module Dummy
   class Application < Rails::Application
@@ -20,4 +18,3 @@ module Dummy
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
