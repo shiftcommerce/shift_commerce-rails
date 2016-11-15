e2d4ad751ebde228c0b4fe8559a42b79636d388a
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-11-15 16:01:13 +0000

    Updated specs

--- spec/concerns/http_caching_spec.rb
+++ spec/concerns/http_caching_spec.rb
@@ -40,7 +40,7 @@ describe ShiftCommerce::HttpCaching, type: :controller do
     it "should set a private Cache-Control header" do
       get :index
 
-      expect(response.header['Cache-Control']).to include('private, max-age=0, must-revalidate')
+      expect(response.header['Cache-Control']).to include('private')
     end
   end
 
@@ -63,7 +63,7 @@ describe ShiftCommerce::HttpCaching, type: :controller do
 
       get :index
 
-      expect(response.header['Cache-Control']).to eq('max-age=0, must-revalidate')
+      expect(response.header['Cache-Control']).to_not include('private')
       expect(response.header['Surrogate-Key']).to eq('foo bar')
       expect(response.header['Surrogate-Control']).to include('max-age=3600,stale-if-error=86400,stale-while-revalidate=86400')
     end
@@ -90,7 +90,7 @@ describe ShiftCommerce::HttpCaching, type: :controller do
 
       expect(response.header['Vary']).to eq('Cookie')
       expect(response.header['Surrogate-Key']).to eq('foo bar')
-      expect(response.header['Cache-Control']).to eq('max-age=0, must-revalidate')
+      expect(response.header['Cache-Control']).to_not include('private')
       expect(response.header['Surrogate-Control']).to include('max-age=3600,stale-if-error=86400,stale-while-revalidate=86400')
     end
   end
