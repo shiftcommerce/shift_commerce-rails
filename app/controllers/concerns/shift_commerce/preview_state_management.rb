643bb4b4d4b9b86bacca203914a59dbae2641ad4
Peter Hicks <peter@hicks.im>
2016-11-23 16:27:33 +0000

    Extract preview state management from Rails frontend.

---
+++ app/controllers/concerns/shift_commerce/preview_state_management.rb
@@ -0,0 +1,31 @@
+#
+# PreviewStateManagement concern
+#
+# This module contains logic for handling the preview state
+#
+module PreviewStateManagement
+  extend ActiveSupport::Concern
+
+  included do
+    # Check for the previewed state.
+    around_action :handle_preview_state
+  end
+
+  protected
+
+  def handle_preview_state
+    if params[:preview].present?
+      begin
+        # Doing this is not particular pretty and raises a question to thread
+        # safety of the underlying gem implementation. This MUST be addressed
+        # later - FIXME
+        FlexCommerceApi::ApiBase.reconfigure_all include_previewed: true
+        yield
+      ensure
+        FlexCommerceApi::ApiBase.reconfigure_all
+      end
+    else
+      yield
+    end
+  end
+end
