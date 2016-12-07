258a48a9e461f51514e1f2a885f48f3911177a75
David McNally <david.mcnally@artofsource.com>
2016-12-07 15:33:32 +0000

    Reverted 28a0dd761859d6925d4119662fe324db97694c89

--- app/helpers/shift_commerce/template_definition_helper.rb
+++ app/helpers/shift_commerce/template_definition_helper.rb
@@ -1,7 +1,7 @@
 module ShiftCommerce
   module TemplateDefinitionHelper
     def render_template_for(item, *args)
-      tpl = item.template
+      tpl = item.template_definition
       if(tpl.present?)
         render "template_definitions/#{tpl.reference}", *args
       else
