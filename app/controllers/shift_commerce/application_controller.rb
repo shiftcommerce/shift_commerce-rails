dcec133919cbd9f598c1b5afc1b1a3db6ae38686
Peter Hicks <peter@hicks.im>
2016-12-12 17:26:19 +0000

    Start extracting static pages from frontend

--- app/controllers/shift_commerce/application_controller.rb
+++ app/controllers/shift_commerce/application_controller.rb
@@ -1,5 +1,53 @@
 module ShiftCommerce
   class ApplicationController < ActionController::Base
+    include ShiftCommerce::HttpCaching
+    include ShiftCommerce::PreviewStateManagement
+    include ShiftCommerce::NotFoundRedirects
+    include ShiftCommerce::AssetPush
+    include ResourceUrl
+    include CartManagement
+    include MenuManagement
+    include AccountManagement
+    include ShippingMethodManagement
+
+    # Set the X-Cart-Id header on all requests with a cart_id session
+    after_action :set_x_cart_id, unless: -> { request.get? || request.head? }
+
+    PUSH_ASSETS = [
+      'application.css'.freeze,
+      'modernizr-custom.js'.freeze
+    ].freeze
+
+    # Prevent CSRF attacks by raising an exception.
     protect_from_forgery with: :exception
+
+    expose(:page_title) { t("application_name") }
+    expose(:canonical_url) { nil }
+
+    # similar regex is also used in rack_slug.rb
+    # this regex will validates old matalan site canonical product urls
+    expose(:old_canonical_sku_regex) { /^\/product\/detail\/(S\d{7}).*$/.freeze }
+
+    # this overrides NotFoundRedirects#handle_not_found to provide site-wide nice 404 pages
+    def handle_not_found(exception = nil)
+      render "shared/not_found".freeze, status: 404
+    end
+
+    private
+
+    # returns an array of assets to be pushed by HTTP/2
+    def push_assets
+      PUSH_ASSETS
+    end
+
+    # Sets the X-Cart-Id header
+    def set_x_cart_id
+      # Do not set on GET/HEAD requests
+      # Only set when a cart is active
+      if session[:cart_id].present?
+        response.headers['X-Cart-Id'.freeze] = session[:cart_id]
+      end
+    end
+
   end
 end
---
+++ app/controllers/shift_commerce/static_pages_controller.rb
@@ -0,0 +1,45 @@
+module ShiftCommerce
+  class StaticPagesController < ShiftCommerce::ApplicationController
+    include ShiftCommerce::TemplateDefinitionHelper
+    cache_shared_page only: :show
+
+    API_STATIC_PAGE_INCLUDES = "template_definition,meta.*".freeze
+
+    expose(:static_page) { fetch_static_page }
+
+    def show
+      set_meta_tags title: static_page_title,
+                    canonical: generate_absolute_url_for(static_page.slug),
+                    description: static_page.meta_attribute(:meta_description),
+                    keywords: static_page.meta_attribute(:keywords)
+
+      if static_page.published == true
+        if request.headers['X-PJAX'.freeze] || params[:minimal]
+          render_template_for static_page, layout: false
+        else
+          render_template_for static_page
+        end
+      else
+        handle_resource_not_found
+      end
+    end
+
+    private
+
+    def redirect_lookup_params
+      if params[:slug_parts].present?
+        { source_type: "static_pages".freeze, source_slug: params[:slug_parts] }
+      else
+        super
+      end
+    end
+
+    def fetch_static_page
+      ::FlexCommerce::StaticPage.includes(API_STATIC_PAGE_INCLUDES).find(params[:id]).first
+    end
+
+    def static_page_title
+      static_page.meta_attribute(:meta_title_override) || static_page.title
+    end
+  end
+end
