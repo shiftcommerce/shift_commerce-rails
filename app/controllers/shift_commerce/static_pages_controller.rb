d806735d5d864621bfc68d8d2cbf1a365edecb07
Peter Hicks <peter@hicks.im>
2017-01-10 15:27:52 +0000

    Update static pages controller to use local variables

--- app/controllers/shift_commerce/static_pages_controller.rb
+++ app/controllers/shift_commerce/static_pages_controller.rb
@@ -7,30 +7,29 @@ module ShiftCommerce
     API_STATIC_PAGE_INCLUDES = "template_definition,meta.*".freeze
 
     def show
-      fetch_static_page
       set_static_page_meta_tags
       render_static_page
     end
 
     private
 
-    def fetch_static_page
-      @static_page = ::FlexCommerce::StaticPage.includes(API_STATIC_PAGE_INCLUDES).find(params[:id]).first
-    end
-
     def set_static_page_meta_tags
-      set_meta_tags title: @static_page.meta_attribute(:meta_title_override) || @static_page.title,
-                    canonical: generate_absolute_url_for(@static_page.slug),
-                    description: @static_page.meta_attribute(:meta_description),
-                    keywords: @static_page.meta_attribute(:keywords)
+      set_meta_tags title: static_page.meta_attribute(:meta_title_override) || static_page.title,
+                    canonical: generate_absolute_url_for(static_page.slug),
+                    description: static_page.meta_attribute(:meta_description),
+                    keywords: static_page.meta_attribute(:keywords)
     end
 
     def render_static_page
-      if @static_page.published == true
-        render_template_for @static_page
+      if static_page.published == true
+        render locals: { static_page: static_page }
       else
         handle_resource_not_found
       end
+    end
+
+    def static_page
+      ::FlexCommerce::StaticPage.includes(API_STATIC_PAGE_INCLUDES).find(params[:id]).first
     end
 
   end
