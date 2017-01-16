0eb89c290cf5052b3d169d955c6708c17c62ff02
David McNally <david.mcnally@artofsource.com>
2017-01-16 12:09:51 +0000

    Add check for archived objects

--- app/controllers/concerns/shift_commerce/check_canonical_path.rb
+++ app/controllers/concerns/shift_commerce/check_canonical_path.rb
@@ -2,7 +2,7 @@ module ShiftCommerce
   module CheckCanonicalPath
 
     def check_canonical_path_for(object)
-      if object.archived
+      if object.respond_to?(:archived) && object.archived
         render 410
       elsif request.path != object.canonical_path
         redirect_to object.canonical_path, status: :moved_permanently
