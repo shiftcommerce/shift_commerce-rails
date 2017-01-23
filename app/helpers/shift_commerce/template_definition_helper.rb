81a79601a1caec945ad2b7a6bb7c467dee6da373
Josh <JoshuaTatterton@users.noreply.github.com>
2017-01-23 10:53:23 +0000

    Update template format (#22)
    * Update template format used and updated ruby gem

--- app/helpers/shift_commerce/template_definition_helper.rb
+++ app/helpers/shift_commerce/template_definition_helper.rb
@@ -1,11 +1,11 @@
 module ShiftCommerce
   module TemplateDefinitionHelper
     def render_template_for(item, *args)
-      if item.template_definition.present?
+      if item.template.present?
         if minimal_layout?
-          render "template_definitions/#{item.template_definition.reference}", *args, layout: false
+          render "template_definitions/#{item.template.reference}", *args, layout: false
         else
-          render "template_definitions/#{item.template_definition.reference}", *args
+          render "template_definitions/#{item.template.reference}", *args
         end
       else
         render *args
