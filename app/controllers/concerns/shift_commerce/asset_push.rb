2dc98503e567fbe93d38dfb1a9199191246f9573
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2017-05-16 09:23:33 +0100

    Initial pseudo code - wip

--- app/controllers/concerns/shift_commerce/asset_push.rb
+++ app/controllers/concerns/shift_commerce/asset_push.rb
@@ -14,13 +14,22 @@ module ShiftCommerce
     def add_preload_headers
       response.headers['Link'] ||= []
       Array(push_assets).each do |path|
-        response.headers['Link'].push("<#{view_context.asset_path(path)}>; rel=preload")
+        response.headers['Link'].push("<#{view_context.asset_path(path)}>; rel=preload; #{asset_type(path)}")
       end
     end
 
     # these should only be non-async assets used in <head>
     def push_assets
       raise NotImplementedError, "to use asset push, you must define #push_assets"
+    end
+
+    def asset_type(path)
+      extention_name = File.extname(path)
+      case extention_name
+      when ".js" then "as=script"
+      when ".css" then "as=stylesheet"
+      else return
+      end
     end
   end
 end
