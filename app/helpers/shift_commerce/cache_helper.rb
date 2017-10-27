e4bfc2105b4a70b11b6d8a5aa41e6da8c888e2f7
Richard Watkins <richardwatkins1@gmail.com>
2017-10-27 10:06:03 +0100

    menu changed to object

--- app/helpers/shift_commerce/cache_helper.rb
+++ app/helpers/shift_commerce/cache_helper.rb
@@ -14,7 +14,7 @@ module ShiftCommerce
     private
 
     def shift_cache_key(object, banner_reference)
-      object_id = banner_reference == nil ? menu.id : banner_reference
+      object_id = banner_reference == nil ? object.id : banner_reference
       [object.class.name, object_id, object.updated_at.to_datetime.utc.to_i.to_s].join("/")
     end
   end
