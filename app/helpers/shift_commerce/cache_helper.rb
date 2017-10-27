11694f795abb6919063a1f894a3b74ef024ae82c
Richard Watkins <richardwatkins1@gmail.com>
2017-10-27 10:00:00 +0100

    updated menus_cache

--- app/helpers/shift_commerce/cache_helper.rb
+++ app/helpers/shift_commerce/cache_helper.rb
@@ -5,16 +5,17 @@ module ShiftCommerce
       cache(shift_cache_key object, *args) { yield }
     end
 
-    def menus_cache(reference)
+    def menus_cache(reference, banner_reference = nil)
       menu = all_menus_cache[reference] || MissingMenu.new(reference)
       return yield if menu.nil?
-      multi_cache(shift_cache_key(menu)) { yield if block_given? }
+      multi_cache(shift_cache_key(menu, banner_reference)) { yield if block_given? }
     end
 
     private
 
-    def shift_cache_key object
-      [object.class.name, object.id, object.updated_at.to_datetime.utc.to_i.to_s].join("/")
+    def shift_cache_key(object, banner_reference)
+      object_id = banner_reference == nil ? menu.id : banner_reference
+      [object.class.name, object_id, object.updated_at.to_datetime.utc.to_i.to_s].join("/")
     end
   end
 end
