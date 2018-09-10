24861c34b1bc0ce3c9172e64f36637ee33211fe1
Prasanna <praWeb@users.noreply.github.com>
2018-09-10 14:14:14 +0100

    Feature/Introduce Dependent caches in menus (#51)
    * Introduce nested level caches
    
    * Refactor menus_cache method

--- app/helpers/shift_commerce/cache_helper.rb
+++ app/helpers/shift_commerce/cache_helper.rb
@@ -5,21 +5,38 @@ module ShiftCommerce
       cache(shift_cache_key object, *args) { yield }
     end
 
-    def menus_cache(reference, banner_reference = nil, options = {})
+    def menus_cache(reference, banner_reference = nil, dependent_reference = nil, options = {})
       # Dont fetch from cache if requested for a preview
       return yield if params[:preview] === 'true'
 
-      # Fetch from cache for normal requests
-      menu = all_menus_cache[reference] || MissingMenu.new(reference)
-      return yield if menu.nil?
-      multi_cache(shift_cache_key(menu, banner_reference), options ) { yield if block_given? }
-    end
+      # Initialize menus variable to an array
+      menus = []
 
+      [reference, dependent_reference].each do |key|
+        menus.push(all_menus_cache[key] || MissingMenu.new(key)) unless key.nil?
+      end
+
+      # If we dont find any in cache, then fetch via api
+      return yield if menus.nil?
+
+      # Fetch from cache for normal requests
+      if dependent_reference.nil?
+        multi_cache(shift_cache_key(menus, banner_reference), options) { yield if block_given? }
+      else
+        multi_dependent_cache(shift_cache_key(menus), options) { yield if block_given? }
+      end
+    end
+  
     private
 
     def shift_cache_key(object, banner_reference = nil)
-      object_id = banner_reference == nil ? object.id : banner_reference
-      [object.class.name, object_id, object.updated_at.to_datetime.utc.to_i.to_s].join("/")
+      keys = []
+      Array(object).each do |menu|
+        object_id = banner_reference == nil ? menu.id : banner_reference
+        keys.push(["FlexCommerce::Menu", object_id, menu.updated_at.to_datetime.utc.to_i.to_s].join("/"))
+      end
+      keys
     end
+
   end
 end
