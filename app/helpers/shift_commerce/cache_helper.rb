0b25268fdf655a8eab9b08ac5742022bd2a57c00
praWeb <prasanna.gaddam@shiftcommerce.com>
2019-06-05 11:29:56 +0100

    introduce cache version

--- app/helpers/shift_commerce/cache_helper.rb
+++ app/helpers/shift_commerce/cache_helper.rb
@@ -5,7 +5,7 @@ module ShiftCommerce
       cache(shift_cache_key object, *args) { yield }
     end
 
-    def menus_cache(reference, banner_reference = nil, dependent_reference = nil, options = {})
+    def menus_cache(reference, cache_version, banner_reference = nil, dependent_reference = nil, options = {})
       # Dont fetch from cache if requested for a preview
       return yield if params[:preview] === 'true'
 
@@ -21,19 +21,19 @@ module ShiftCommerce
 
       # Fetch from cache for normal requests
       if dependent_reference.nil?
-        multi_cache(shift_cache_key(menus, banner_reference), options) { yield if block_given? }
+        multi_cache(shift_cache_key(menus, cache_version, banner_reference), options) { yield if block_given? }
       else
-        multi_dependent_cache(shift_cache_key(menus), options) { yield if block_given? }
+        multi_dependent_cache(shift_cache_key(menus, cache_version), options) { yield if block_given? }
       end
     end
 
     private
     # resource can be a FlexCommerce::Menu, FlexCommerce::StaticPage or any other resource we wish to cache.
-    def shift_cache_key(resource, banner_reference = nil)
+    def shift_cache_key(resource, cache_version, banner_reference = nil)
       keys = []
       Array(resource).each do |resource|
         resource_id = banner_reference == nil ? resource.id : banner_reference
-        keys.push([resource.class, resource_id, resource.updated_at.to_datetime.utc.to_i.to_s].join("/"))
+        keys.push([resource.class, resource_id, cache_version, resource.updated_at.to_datetime.utc.to_i.to_s].join("/"))
       end
       keys
     end
